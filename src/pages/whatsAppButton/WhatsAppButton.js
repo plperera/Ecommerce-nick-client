122cbff68f1d995be84d1b3fd27e1a48cd85b17d
Pedro Pereira <plp.leoncio@gmail.com>
2023-11-01 16:23:47 -0300

    feat: feito botão que direciona para o WhatsApp

--- src/pages/whatsAppButton/WhatsAppButton.js
+++ src/pages/whatsAppButton/WhatsAppButton.js
@@ -1,46 +1,45 @@
-import styled from "styled-components"
-import MenuComponent from "../../components/menu/Menu"
-import SubMenuComponent from "../../components/menu/SubMenu/SubMenu"
-import { useLocation } from "react-router-dom";
-import CheckoutMenu from "../../components/menu/CheckoutMenu/CheckoutMenu";
-import { useState } from "react";
+import styled, { keyframes } from "styled-components"
+import { BsWhatsapp } from 'react-icons/bs';
 
 export default function WhatsAppButton () {
-
-    const location = useLocation();
-    const isAdminRoute = location.pathname.includes("/admin");
-    const isCheckoutRoute = location.pathname.includes("/checkout");
-
-    const [ expandMenu, setExpandMenu ] = useState(false)
-
+    const msg = ""
+    const whatsAppNumber = "+5511985546210"
     return(
-        isAdminRoute?(
-        <AdminMenuContainer></AdminMenuContainer>
-        ):(
-            isCheckoutRoute?(
-                <CheckoutMenu/>
-            ):(
-                <UserMenuContainer>
-                    <MenuComponent setExpandMenu={setExpandMenu}/>
-                    <SubMenuComponent expandMenu={expandMenu} setExpandMenu={setExpandMenu}/>
-                </UserMenuContainer>
-            )
-            
-        )
-        
+        <Container>
+            <a href={`https://wa.me/${whatsAppNumber}?text=${encodeURIComponent(msg)}`} target="_blank" rel="noopener noreferrer"> 
+                <BsWhatsapp/>
+            </a>   
+        </Container>
     )
 }
-
-const UserMenuContainer = styled.div`
-    width: 100%;
-    //height: 14vh;
-    height: 10.5vh;
-    z-index: 9;
-    position: fixed;
-    top: 0;
-    @media (max-width: 850px) {
-        height: auto;
+const buttonAnimation = keyframes`
+    0% { 
+        transform: translateY(0px); 
+    }
+    2% { 
+        transform: rotate(5deg); 
+    }
+    4% { 
+        transform: rotate(-5deg); 
+    }
+    6% { 
+        transform: translateY(0px); 
     }
 `
-const AdminMenuContainer = styled.div`
+const Container = styled.div`
+    position: fixed;
+    display: flex;
+    right: 2vw;
+    bottom: 2vh;
+    z-index: 99;
+    cursor: pointer;
+    animation: ${buttonAnimation} 6s linear infinite;
+    svg {
+        padding: 8px;
+        font-size: 50px;
+        color: #FFFFFF;
+        background-color: #46c256;
+        border-radius: 5px;
+    }
 `
+;
